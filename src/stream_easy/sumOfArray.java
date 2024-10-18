cf1c8a6a045df848a97a20ae431dd883d531775d
Ankit Tamboli <atankittamboli@gmail.com>
2024-10-18 17:18:08 +0530

    name changes of package

---
+++ src/stream_easy/sumOfArray.java
@@ -0,0 +1,24 @@
+package stream_easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class sumOfArray {
+
+	public static void main(String[] args) {
+		
+	//java 7
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        System.out.println("Sum: " + sum);
+ 
+        // java 8
+        int sumForjava8 = numbers.stream().mapToInt(i -> i).sum();
+        System.out.println("sumForjava8: " + sumForjava8);
+        
+	}
+
+}
