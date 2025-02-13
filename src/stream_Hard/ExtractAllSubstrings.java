4762d209ab684783157b8fb8fa1db359a4242114
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-13 23:11:02 +0530

    13-feb-2025

---
+++ src/stream_Hard/ExtractAllSubstrings.java
@@ -0,0 +1,19 @@
+package stream_Hard;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExtractAllSubstrings {
+
+	public static void main(String[] args) {
+		
+
+//		1:- Extract All Substrings: Given a list of strings,
+//		use flatMap to extract all substrings of length 2 or more.
+		
+		List<String> list = Arrays.asList("A", "Bb", "CcC", "aBC", "Ac", "Ab",
+				"EI", "YOU");
+
+	}
+
+}
