30bba07f929926ab87548632676f0e59edff5157
Ankit Tamboli <atankittamboli@gmail.com>
2025-02-14 23:41:26 +0530

    14-feb-2025

---
+++ src/stream_Medium/FrequencyOfEachWord.java
@@ -0,0 +1,21 @@
+package stream_Medium;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FrequencyOfEachWord {
+
+	public static void main(String[] args) {
+
+//		 List<String> words = List.of("apple banana apple", "banana cherry", "apple banana cherry");
+	        
+	        // Find the frequency of each word
+//	        Map<String, Long> wordFrequency = words.stream()
+//	                .flatMap(line -> List.of(line.split("\\s+")).stream()) // Split each line into words
+//	                .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting())); // Group and count each word
+//	        
+//	        System.out.println(wordFrequency);				
+	}
+
+}
